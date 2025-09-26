cd56ede2ba560969830d1084eedc1b2ce5c65670
Will Cousin <wcousin@gmail.com>
2025-09-25 19:01:40 -0500

    Update Sidebar component

--- frontend/src/components/layout/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar.tsx
@@ -187,7 +187,7 @@ const Sidebar: React.FC<SidebarProps> = ({
       <div className={`
         fixed top-0 left-0 h-full w-80 bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-50
         ${isOpen ? 'translate-x-0' : '-translate-x-full'}
-        md:relative md:translate-x-0 md:shadow-none md:border-r md:w-64
+        md:relative md:translate-x-0 md:shadow md:w-64
         ${className}
       `}>
         <div className="h-full flex flex-col">
